ca9211871455797c253e5dd5bf98ea9601347cd1
youngsterxyf <sas.198708@gmail.com>
2012-11-12 02:13:23 +0800

    使用go fmt格式化了一下autoUpdateHosts.go

--- autoUpdateHosts.go
+++ autoUpdateHosts.go
@@ -3,44 +3,44 @@ package main
 import (
 	"flag"
 	"fmt"
-	"net/http"
 	"io/ioutil"
-	"time"
+	"net/http"
 	"os"
 	"runtime"
+	"time"
 )
 
-func main(){
+func main() {
 
 	sourceUrl := flag.String("-u", "https://smarthosts.googlecode.com/svn/trunk/hosts", "The url where hosts content comes from")
 	absolutePath := flag.String("-t", "C://windows/system32/drivers/etc/hosts", "The absolute path which locates the hosts file")
-    targetPath := *absolutePath
-    if runtime.GOOS == "linux" {
+	targetPath := *absolutePath
+	if runtime.GOOS == "linux" {
 		targetPath = "/etc/hosts"
 	}
-    fmt.Printf("sourceUrl: %s\n", *sourceUrl)
-    fmt.Printf("hostsPath: %s\n", targetPath)
+	fmt.Printf("sourceUrl: %s\n", *sourceUrl)
+	fmt.Printf("hostsPath: %s\n", targetPath)
 	resp, err := http.Get(*sourceUrl)
 	if err != nil {
-        fmt.Println("Status: http.Get Error")
+		fmt.Println("Status: http.Get Error")
 		return
 	}
 
 	file, err := os.Create(targetPath)
-	if err != nil{
-        fmt.Println("Status: os.Create Error")
+	if err != nil {
+		fmt.Println("Status: os.Create Error")
 		return
 	}
 	defer resp.Body.Close()
 	defer file.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-        fmt.Println("Status: ioutil.ReadAll Error")
+		fmt.Println("Status: ioutil.ReadAll Error")
 		return
 	}
 	strBody := string(body)
 	strTime := time.Now().String()
 	strBody = "# " + strTime + "\n127.0.0.1\tkubuntu-dell\n" + strBody
 	file.WriteString(strBody)
-    fmt.Println("Status: Update Successfully!")
+	fmt.Println("Status: Update Successfully!")
 }
